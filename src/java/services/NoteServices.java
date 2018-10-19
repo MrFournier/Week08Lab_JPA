4b898d156b0682b147f103a7a9678681da248b76
ACDM\766375 <troy_fournier@live.com>
2018-10-19 16:41:09 -0600

    Implemented Majority of note DB things

--- src/java/services/NoteServices.java
+++ src/java/services/NoteServices.java
@@ -5,10 +5,47 @@
  */
 package services;
 
+import database.NoteDB;
+import database.NotesDBException;
+import java.util.Date;
+import java.util.List;
+import models.Note;
+
 /**
  *
  * @author 766375
  */
 public class NoteServices {
     
+    private NoteDB noteDB;
+    
+    public NoteServices() {
+        noteDB = new NoteDB();
+    }
+    
+    public Note get(int noteId) throws NotesDBException {
+        return noteDB.getNote(noteId);
+    }
+    
+    public List<Note> getAll() throws NotesDBException {
+        return noteDB.getAll();
+    }
+    
+    public int update(int noteId, String contents) throws NotesDBException {
+        Note note = noteDB.getNote(noteId);
+        note.setContent(contents);
+        return noteDB.update(note);
+    }
+    
+    public int delete(int noteId) throws NotesDBException {
+        Note deletedNote = noteDB.getNote(noteId);
+        return noteDB.delete(deletedNote);
+    }
+    
+    public int insert(String contents) throws NotesDBException {
+        // Repsonsible for creating the date
+        Note newNote = new Note(0, contents, new Date());
+        return noteDB.insert(newNote);
+    }
+
 }
